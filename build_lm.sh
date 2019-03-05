3528149a0a1de54b1949404eb228be7fba85d2e1
Giovanni De Toni <giovanni.det@gmail.com>
2019-03-05 10:54:13 +0100

    Add unknown words token and frequency cut-off.

--- build_lm.sh
+++ build_lm.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/env bash
-"""
-Script used to build a language model given OpenFST tool
-and train/test data files. It also computes the perplexity
-over the test dataset.
-
-Author: Giovanni De Toni
-Date: 05/03/2019
-Email: giovanni.det@gmail.com
-"""
+# Script used to build a language model given OpenFST tool
+# and train/test data files. It also computes the perplexity
+# over the test dataset.
+#
+# Author: Giovanni De Toni
+# Date: 05/03/2019
+# Email: giovanni.det@gmail.com
 
 export train_data=""
 export test_data=""
@@ -44,10 +42,16 @@ if [ -z $ngrams ]; then ngrams=1; fi
 if [ -z $method ]; then method="witten_bell"; fi
 
 # Run fst on the model
+echo "[*] Generating the LM"
 ngramsymbols $train_data > lex.txt
-farcompilestrings --symbols=lex.txt -keep_symbols=1 $train_data > text.far
+farcompilestrings --symbols=lex.txt --unknown_symbol="<unk>" -keep_symbols=1 $train_data > text.far
+
+# Compute the ngrams and do frequency cutoff
 ngramcount --order="$ngrams" text.far > text.cnts
-ngrammake --method="$method" text.cnts > text.lm
+ngramshrink --method="count_prune" --count_pattern=1:2 text.cnts > text_reduced.cnts
+
+# Build the actual LM
+ngrammake --method="$method" text_reduced.cnts > text.lm
 
 # Generating a random string
 echo "[*] Generating a random string from the LM"
@@ -57,10 +61,10 @@ echo "[*] The string is: $generated_string"
 # Compute the perplexity on the test data
 echo "[*] Computing perplexity on the given test dataset $test_data"
 ngramsymbols $test_data > test.txt
-farcompilestrings --symbols=test.txt -keep_symbols=1 $test_data > test.far
-ngramperplexity text.lm test.far
+farcompilestrings --symbols=lex.txt --unknown_symbol="<unk>" $test_data > test.far
+ngramperplexity --OOV_symbol="<unk>" text.lm test.far
 
 # Clean the directory from the generated files
-rm lex.txt text.far text.cnts text.lm test.txt test.far
+rm lex.txt text.far text.cnts text.lm text_reduced.cnts test.txt test.far
 
 
