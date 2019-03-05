796fb2433e22614010212cdddd37fd86cf01d55a
Giovanni De Toni <giovanni.det@gmail.com>
2019-03-05 10:31:47 +0100

    Add script to build a LM using OpenFST.

---
+++ build_lm.sh
@@ -0,0 +1,66 @@
+#!/usr/bin/env bash
+"""
+Script used to build a language model given OpenFST tool
+and train/test data files. It also computes the perplexity
+over the test dataset.
+
+Author: Giovanni De Toni
+Date: 05/03/2019
+Email: giovanni.det@gmail.com
+"""
+
+export train_data=""
+export test_data=""
+export ngrams=""
+export method=""
+
+# Usage and version information
+eval "$(docopts -V - -h - : "$@" <<EOF
+Usage: compute_lm [-nc <ngrams>] [-m <method>] [<train_data>] [<test_data>]
+
+Options:
+	<train_data>    Train dataset used.
+	<test_data>     Test dataset used.
+	-nc <ngrams>    Size of n-grams.
+	-m <method>     Method used for discounting.
+	--help			Show help options.
+	--version		Print program version.
+----
+compute_lm 0.1.0
+Copyright (C) 2019 Giovanni De Toni
+License MIT
+This is free software: you are free to change and redistribute it.
+There is NO WARRANTY, to the extent permitted by law.
+EOF
+)"
+
+# Unofficial strict bash
+set -euo pipefail
+IFS=$'\n\t'
+
+if [ -z $train_data ]; then train_data="NL2SparQL4NLU/dataset/NL2SparQL4NLU.train.utterances.txt"; fi
+if [ -z $test_data ]; then test_data="NL2SparQL4NLU/dataset/NL2SparQL4NLU.test.utterances.txt"; fi
+if [ -z $ngrams ]; then ngrams=1; fi
+if [ -z $method ]; then method="witten_bell"; fi
+
+# Run fst on the model
+ngramsymbols $train_data > lex.txt
+farcompilestrings --symbols=lex.txt -keep_symbols=1 $train_data > text.far
+ngramcount --order="$ngrams" text.far > text.cnts
+ngrammake --method="$method" text.cnts > text.lm
+
+# Generating a random string
+echo "[*] Generating a random string from the LM"
+generated_string=`ngramrandgen text.lm | farprintstrings`
+echo "[*] The string is: $generated_string"
+
+# Compute the perplexity on the test data
+echo "[*] Computing perplexity on the given test dataset $test_data"
+ngramsymbols $test_data > test.txt
+farcompilestrings --symbols=test.txt -keep_symbols=1 $test_data > test.far
+ngramperplexity text.lm test.far
+
+# Clean the directory from the generated files
+rm lex.txt text.far text.cnts text.lm test.txt test.far
+
+
